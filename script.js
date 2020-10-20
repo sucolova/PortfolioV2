d46ed5591642f2ccafcc59bb8a3dbdff7409ee79
Lukas Melcher <lukas.melcher@gmail.com>
2020-10-20 22:08:35 +0200

    mouse events for gallery

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 const displayedImage = document.querySelector(".displayed-img");
+
+
+displayedImage.addEventListener("mouseenter", function (event) {
+    event.target.setAttribute("src", "img/Gallery/pic2.jpg")
+});
+
+displayedImage.addEventListener("mouseleave", function (event) {
+    event.target.setAttribute("src", "img/Gallery/pic1.jpg")
+});
+
+
+
