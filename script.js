dd3f2477992b43a014bfdf31f72f5a8605f298a5
lukas <lukas.melcher@gmail.com>
2020-10-25 23:36:25 +0100

    javascript gallery is functional

--- script.js
+++ script.js
@@ -17,6 +17,30 @@ const allDisplayedImages = document.querySelectorAll(".displayedImage");
 */
 
 
+const gallery = document.querySelector(".cropped-img");
+
+let croppedImages = ["berg", "stadt", "abstrakt", "natur"];
+
+
+for (let i = 0; i < croppedImages.length; i++){
+    const newImage = document.createElement("img");
+    newImage.setAttribute("src", "img/Gallery/" + croppedImages[i] + ".jpg");
+    newImage.setAttribute("class", "displayedImage");
+    newImage.setAttribute("id",croppedImages[i]) 
+    gallery.appendChild(newImage);
+
+    let displayedImage = document.querySelector("#" + croppedImages[i]);
+
+displayedImage.addEventListener("mouseenter", function (event) {
+    event.target.setAttribute("src", "img/Gallery/" + croppedImages[i] + "1.jpg")
+});
+
+displayedImage.addEventListener("mouseleave", function (event) {
+    event.target.setAttribute("src", "img/Gallery/" + croppedImages[i] + ".jpg")
+});
+}
+
+
 
 
 
