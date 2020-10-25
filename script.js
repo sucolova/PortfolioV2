b0aee1b40ff0854f5fc94facb97177b4b607df6e
lukas <lukas.melcher@gmail.com>
2020-10-25 21:50:03 +0100

    wieder einmal eine lange Nacht

--- script.js
+++ script.js
@@ -1,13 +1,25 @@
-const displayedImage = document.querySelector(".displayed-img");
 
+
+/*
+const displayedImage = document.querySelector(".displayedImage");
 
 displayedImage.addEventListener("mouseenter", function (event) {
-    event.target.setAttribute("src", "img/Gallery/pic2.jpg")
+    event.target.setAttribute("src", "img/Gallery/stadt2.jpg")
 });
 
 displayedImage.addEventListener("mouseleave", function (event) {
-    event.target.setAttribute("src", "img/Gallery/pic1.jpg")
+    event.target.setAttribute("src", "img/Gallery/stadt1.jpg")
 });
+
+*/
+/* war eine gute Idee, geht aber einfacher: erstelle die Gallerie mit javascript füge sie dann in Dom ein.
+const allDisplayedImages = document.querySelectorAll(".displayedImage");
+*/
+
+
+
+
+
 
 
 
